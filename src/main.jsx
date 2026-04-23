5a66883f52d073592008aafe703c948b5282126a
oleksandraAntoniuk <oleksandra.antoniuk.pp.2023@lpnu.ua>
2026-04-23 21:01:56 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import posthog from 'posthog-js'
+
+posthog.init('phc_koMNeG6EEv5fsZ6iKYCMDLPiugUfpdEJ44L5jaW6KH5f', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
